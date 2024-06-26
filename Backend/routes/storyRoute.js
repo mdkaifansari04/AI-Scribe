f0c6a1b6a2367c92a2a6b895225f3d2751a00092
Mdkaif-123 <amdkaif843@gmail.com>
2024-06-26 11:28:15 +0530

    🔥 refactor: update AI model from open-ai to groq

--- Backend/routes/storyRoute.js
+++ Backend/routes/storyRoute.js
@@ -1,13 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/storyController");
+const { verifyUserToken } = require("../middleware/authentiaction");
 
-const express = require("express")
-const router = express.Router()
-const controller = require("../controllers/storyController")
-const { verifyUserToken } = require("../middleware/authentiaction")
+router.get("/top-voted-stories", verifyUserToken, controller.getStories);
+router.post("/generate", controller.generateStory);
+router.post("/save", verifyUserToken, controller.saveStory);
+router.put("/update/:id", verifyUserToken, controller.updateStory);
+router.delete("/delete/:id", verifyUserToken, controller.deleteStory);
 
-router.get('/top-voted-stories', verifyUserToken, controller.getStories)
-router.post('/generate',verifyUserToken, controller.generateStory)
-router.post('/save',verifyUserToken, controller.saveStory)
-router.put('/update/:id',verifyUserToken, controller.updateStory)
-router.delete('/delete/:id',verifyUserToken, controller.deleteStory)
-
-module.exports = router
+module.exports = router;
